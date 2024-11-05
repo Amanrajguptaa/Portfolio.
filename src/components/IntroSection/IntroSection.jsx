ddd404bd6965b4316070b0511df4d2e771dd022e
Aman Raj <rajaman9355@gmail.com>
2024-11-05 16:00:16 +0530

    added resume

--- src/components/IntroSection/IntroSection.jsx
+++ src/components/IntroSection/IntroSection.jsx
@@ -16,9 +16,11 @@ const IntroSection = () => {
           <p className="font-eyesome text-yellow-400 text-2xl md:text-3xl mt-3 italic hover:text-purple-400 transition-colors duration-300">
             "Bringing visions to life through the language of code"
           </p>
+          <a href="https://drive.google.com/file/d/16FS3bj3sCs_xMiHPhIVk2yklqU3Sdk_Q/view?usp=drive_link" target="_blank">
           <button className=" text-sm md:text-lg mt-5 bg-gradient-to-r from-purple-600 to-purple-400 rounded-3xl px-6 py-3 font-semibold hover:scale-105 transition-transform duration-300 hover:shadow-lg hover:shadow-purple-500/50">
             Download Resume
           </button>
+          </a>
         </div>
 
         <img
