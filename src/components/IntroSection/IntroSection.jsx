2888fcfe1f4392a296a9c8d8a0f4b251549c966f
Aman Raj Gupta <152897024+Amanrajguptaa@users.noreply.github.com>
2025-03-26 16:43:32 +0530

    Added Updated Resume

--- src/components/IntroSection/IntroSection.jsx
+++ src/components/IntroSection/IntroSection.jsx
@@ -16,7 +16,7 @@ const IntroSection = () => {
           <p className="font-eyesome text-yellow-400 text-2xl md:text-3xl mt-3 italic hover:text-purple-400 transition-colors duration-300">
             "Bringing visions to life through the language of code"
           </p>
-          <a href="https://drive.google.com/file/d/16FS3bj3sCs_xMiHPhIVk2yklqU3Sdk_Q/view?usp=drive_link" target="_blank">
+          <a href="https://drive.google.com/file/d/1NQ4SXDmHqY2WH5sOLTuPwaUcFrWrjkch/view?usp=sharing" target="_blank">
           <button className=" text-sm md:text-lg mt-5 bg-gradient-to-r from-purple-600 to-purple-400 rounded-3xl px-6 py-3 font-semibold hover:scale-105 transition-transform duration-300 hover:shadow-lg hover:shadow-purple-500/50">
             Download Resume
           </button>
